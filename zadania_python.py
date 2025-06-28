e495071fad7cf858f6b9c2dcce6e2c90482b604d
karolmatczuk <karol.matczuk97@gmail.com>
2025-06-28 17:37:54 +0200

    Update zadania_python.py

--- zadania_python.py
+++ zadania_python.py
@@ -6,4 +6,17 @@ def czy_podzielna(a, b):
         return True
     else:
         return False
+
+
 # Zadanie 2
+def liczby_pierwsze(n):
+    l_pierwsze = []
+    for num in range(2, n):
+        jest_pierwsza = True
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                jest_pierwsza = False
+                break
+        if jest_pierwsza:
+            l_pierwsze.append(num)
+    return l_pierwsze
