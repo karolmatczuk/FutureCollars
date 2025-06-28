961877201f28af14bc48ce06fe25ca3fe78b764e
karolmatczuk <karol.matczuk97@gmail.com>
2025-06-28 17:30:10 +0200

    Update zadania_python.py

--- zadania_python.py
+++ zadania_python.py
@@ -1,1 +1,9 @@
-
+# Zadanie 1
+def czy_podzielna(a, b):
+    if b == 0:
+        return "Nie można dzielić przez zero!"
+    if a % b == 0:
+        return True
+    else:
+        return False
+# Zadanie 2
